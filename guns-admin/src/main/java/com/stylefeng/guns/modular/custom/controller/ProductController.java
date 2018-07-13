a1fa3405b9e79d0770bf877578cdf0fae10827f1
gq <guanqinghz@163.com>
2018-07-13 10:37:51 +0800

    xb

--- guns-admin/src/main/java/com/stylefeng/guns/modular/custom/controller/ProductController.java
+++ guns-admin/src/main/java/com/stylefeng/guns/modular/custom/controller/ProductController.java
@@ -5,6 +5,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import java.text.SimpleDateFormat;
@@ -68,6 +70,17 @@ public class ProductController extends BaseController {
     @ResponseBody
     public Object list(String condition) {
         return productService.selectList(null);
+    }
+    
+    /**
+     * 表单提交日期绑定
+     * @param binder
+     */
+    @InitBinder
+    public void initBinder(WebDataBinder binder) {
+    	SimpleDateFormat  sdf = new SimpleDateFormat("yyyy-MM-dd");
+    	sdf.setLenient(false);
+    	binder.registerCustomEditor(Date.class, new CustomDateEditor(sdf, true));
     }
 
     /**
