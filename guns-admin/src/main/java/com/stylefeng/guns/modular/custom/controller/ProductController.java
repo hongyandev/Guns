0d5b50b61aaec9890a07bf2f6b2d8ee1da3ebe1c
gq <guanqinghz@163.com>
2018-07-13 09:50:55 +0800

    完成修改

--- guns-admin/src/main/java/com/stylefeng/guns/modular/custom/controller/ProductController.java
+++ guns-admin/src/main/java/com/stylefeng/guns/modular/custom/controller/ProductController.java
@@ -5,8 +5,16 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.PathVariable;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.propertyeditors.CustomDateEditor;
+
 import com.stylefeng.guns.core.log.LogObjectHolder;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -72,6 +80,17 @@ public class ProductController extends BaseController {
     public Object add(Product product) {
         productService.insert(product);
         return SUCCESS_TIP;
+    }
+    
+    /**
+     * 表单提交日期绑定
+     * @param binder
+     */
+    @InitBinder
+    public void initBinder(WebDataBinder binder) {
+    	SimpleDateFormat  sdf = new SimpleDateFormat("yyyy-MM-dd");
+    	sdf.setLenient(false);
+    	binder.registerCustomEditor(Date.class, new CustomDateEditor(sdf, true));
     }
 
     /**
