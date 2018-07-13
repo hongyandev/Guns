b50c413f96ab951cc3a3beb3de22584fdc991c7f
毛袁晨 <maoyuanchen@gmail.com>
2018-07-13 09:32:39 +0800

    lll

--- guns-admin/src/main/java/com/stylefeng/guns/config/properties/AliyunProperties.java
+++ guns-admin/src/main/java/com/stylefeng/guns/config/properties/AliyunProperties.java
@@ -1,12 +1,41 @@
 package com.stylefeng.guns.config.properties;
 
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Configuration;
+import java.util.Map;
 
-@Configuration
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+import com.google.common.collect.Maps;
+/**
+ * 阿里云配置
+ * @author myc
+ *
+ */
+@Component
 @ConfigurationProperties(prefix = AliyunProperties.ALIYUNCONF_PREFIX)
 public class AliyunProperties {
 	public static final String ALIYUNCONF_PREFIX = "aliyun";
 	
+	String isDev;
 	
+	Map<String, Object> livingConf = Maps.newHashMap();
+	
+	public String getLivingApiHost() {
+		return (String) livingConf.get("apiHost");
+	}
+	
+	@SuppressWarnings("unchecked")
+	public String getLivingApiVer(String key) {
+		return (String) ((Map<String,Object>) livingConf.get("apiVer")).get(key);
+	}
+	
+	@SuppressWarnings("unchecked")
+	public String getLivingAppKey() {
+		return (String) ((Map<String,Object>) livingConf.get("appKey")).get(isDev);
+	}
+	
+	@SuppressWarnings("unchecked")
+	public String getLivingAppSecret() {
+		return (String) ((Map<String,Object>) livingConf.get("appSecret")).get(isDev);
+	}
 }
